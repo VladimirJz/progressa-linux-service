b1454828c29d810e0a2c2ee5f31d6022789a21c7
Vladimir <vladimir.jz@hotmail.com>
2022-11-11 16:53:28 -0600

    add : raw output

--- lib/database.py
+++ lib/database.py
@@ -66,4 +66,135 @@ class Repository():
        },
     ]
     
+    Cartera =[
+            {'routine':'VENCIMIENTOSREP',
+            'keyword':'vencimientos',
+            'output':'table',
+            'parameters':
+                        [{  'order':1,
+                            'name':'FechaInicio',
+                            'type': str,
+                            'default':'1900-01-01',# Global
+                            'required':True 
+                        },
+                        {  'order':2,
+                            'name':'FechaFin',
+                            'type':str,
+                            'default':'1900-01-01',# Global
+                            'required':True 
+                        },
+                        {  'order': 3,
+                            'name':'SucursalID',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order': 4,
+                            'name':'Moneda',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order': 5,
+                            'name':'ProductoID',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order':6,
+                            'name':'PromotorID',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order':7,
+                            'name':'Genero',
+                            'type':str,
+                            'default':"",# Global
+                            'required':False 
+                        },
+                        {  'order':8,
+                            'name':'EstadoID',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order':9,
+                            'name':'MunicipioID',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order':10,
+                            'name':'MinDiasAtraso',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order':11,
+                            'name':'MaxDiasAtraso',
+                            'type':int,
+                            'default':99999,# Global
+                            'required':False 
+                        },
+                        {  'order':12,
+                            'name':'InstNominaID',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order':13,
+                            'name':'ConvenioID',
+                            'type':int,
+                            'default':0,# Global
+                            'required':False 
+                        },
+                        {  'order':14,
+                            'name':'EmpresaID',
+                            'type':int,
+                            'default':1,# Global
+                            'required':False 
+                        },
+                        {  'order':15,
+                            'name':'Usuario',
+                            'type':int,
+                            'default':1,# Global
+                            'required':False 
+                        },
 
+                      {  'order':16,
+                            'name':'FechaActual',
+                            'type':str,
+                            'default':'1900-01-01',# Global
+                            'required':False 
+                        },
+                      {  'order':17,
+                            'name':'DireccionIP',
+                            'type':str,
+                            'default':'127.0.01',# Global
+                            'required':False 
+                        },
+
+                      {  'order':18,
+                            'name':'ProgramaID',
+                            'type':str,
+                            'default':1,# Global
+                            'required':False 
+                        },
+
+                      {  'order':19,
+                            'name':'Sucursal',
+                            'type':int,
+                            'default':1,# Global
+                            'required':False 
+                        },                   
+                        {  'order':20,
+                            'name':'Transaccion',
+                            'type':int,
+                            'default':1,# Global
+                            'required':False 
+                        },
+                        
+                        ]
+            }
+            ]
--- lib/safi.py
+++ lib/safi.py
@@ -132,29 +132,52 @@ class Session():
             return data_no_headers
             pass
         
+        def fetch_raw(resultset):
+            '''
+            Devuelve el resultset en formato de lista sin encabezados.
+            '''
+            print("raw");
+            results=[]
+            print(type(resultset))
+            for a in resultset:
+                print(type(a))
+                print(a)
+                results.append(a)
+            return results
+
         params=request.parameters
         routine=request.routine
         resultset=self._run(routine,params)
+        print("AQUI")
         if resultset:
+                
             #print(type(resultset))
             if format=='json':
+                print("JS")
                 return json_str(resultset)
             if format=='raw':
-                return  resultset
+                return  fetch_raw(resultset)
             if format=='onlydata':
+                print("OD")
                 return only_data(resultset)
-                pass
-            raw_data = self.fetch_raw(resultset) 
+                
+            if format=='raw':
+                print("RW")
+                return fetch_raw(resultset) 
         else:
             message='API: No Rows to return. '  
             logger.info(message)
             return None
-
+        raw_data=resultset
         return raw_data
 
 
         
-    def fetch_raw(self, cursor):
+
+
+
+    def fetch_raw_old(self, cursor):
+
         columns = [col[0] for col in cursor.description]
         #print (cursor.rowcount)
         #print (columns)
@@ -173,7 +196,7 @@ class Session():
                 results=dict(zip(columns, row))
             #print(row)
             #print(results)
-        return results
+        return cursor.fetchall()
 
     
     def is_connected(self):
@@ -435,6 +458,12 @@ class Request():
             super().__init__(request)
             repository=Repository.Integracion
             self.properties=self.get_props(request,repository)
+    
+    class Cartera(GenericRequest):
+        def __init__(self,request):
+            super().__init__(request)
+            repository=Repository.Cartera
+            self.properties=self.get_props(request,repository)
 
 
 
@@ -566,8 +595,10 @@ class Generic():
             # convert it to a string
             if isinstance(obj, Decimal):
                 return str(obj)
+
             if isinstance(obj, datetime):
-                return str(obj)
+                return obj.isoformat()
+
             if isinstance(obj, date):
                 return str(obj)
    
